43b7f7917cc8da648a90298561be75419433a5fd
Fakorede Abiola <abiolafakorede@gmail.com>
2024-03-19 12:42:31 -0500

    Fix issue with min car api def

--- automotive/src/main/java/com/thefabdev/androidauto/MyCarAppScreen.java
+++ automotive/src/main/java/com/thefabdev/androidauto/MyCarAppScreen.java
@@ -3,7 +3,7 @@ package com.thefabdev.androidauto;
 import androidx.annotation.NonNull;
 import androidx.car.app.CarContext;
 import androidx.car.app.Screen;
-import androidx.car.app.model.Action;
+// import androidx.car.app.model.Action;
 import androidx.car.app.model.Pane;
 import androidx.car.app.model.PaneTemplate;
 import androidx.car.app.model.Row;
@@ -20,7 +20,8 @@ public class MyCarAppScreen extends Screen {
         Row row = new Row.Builder().setTitle("Hello world!").addText("My Car App").build();
         Pane pane = new Pane.Builder().addRow(row).build();
         return new PaneTemplate.Builder(pane)
-                .setHeaderAction(Action.APP_ICON)
+                .setTitle("Something here...")
+                //.setHeaderAction(Action.APP_ICON)
                 .build();
     }
 }
